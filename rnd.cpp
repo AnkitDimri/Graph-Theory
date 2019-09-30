a1cf666a8af73e2d2c37802c6daf9d28c59946d4
Ankit <ankitdimri1999@gmail.com>
2019-09-30 19:43:07 +0530

    rnd.cpp commented

--- rnd.cpp
+++ rnd.cpp
@@ -12,35 +12,38 @@
 #include <queue>
 #include <algorithm>
 
+ /* function to find the eccentricity of all vertices in a graph */
  std::vector <int> eccentricity  (std::vector <std::vector <int>>&);
+
+ /* Finding the radius and diameter of graph */
  void rnd (std::vector <int> &);
 
  int main(int argc, char const *argv[]) {
 
      int edg = 0, ver = 0, u, v;
-     std::vector <int> E;
+     std::vector <int> E; // Holding the eccentricity of each vertex
 
      std::cout << "\n\n\t Enter the number of vertices and edges in the graph :";
-     std::cin >> ver >> edg;
+     std::cin >> ver >> edg; // number of vertices and edges
 
+     /* Data structure to hold the Adjacency list of the input graph */
      std::vector <std::vector <int>> G (ver, std::vector <int> (0));
-
-     //std::cout << G [0].size () << '\n';
 
      std::cout << "\n\t Enter the vertex pair between which an edge exists :" << '\n';
 
      for (int i = 0; i < edg; i++) {
-         std::cin >> u >> v;
+         std::cin >> u >> v; // enter edge
+         /* Convesion to array numbers */
          u--;
          v--;
+         /* Push them into the adjaency list */
          G [u].push_back (v);
          G [v].push_back (u);
      }
 
-     //std::cout << G [2].size () << '\n';
-
+     /* Find the eccentricities */
      E = eccentricity (G);
-     rnd (E);
+     rnd (E); // find the radius and diameter
 
      return 0;
  }
@@ -48,20 +51,24 @@
  std::vector <int> eccentricity (std::vector <std::vector <int>> &g) {
 
      int ecc = 0, t = 0, v;
-     std::queue <int> q, temp;
-     std::vector <int> E (g.size ());
+     std::queue <int> q, temp; // queue to perform BFS
+     std::vector <int> E (g.size ()); // Data structure to hold values
 
      for (int i = 0; i < g.size (); i++) {
 
-         ecc = 0;
-         std::vector <bool> check (g.size (), 0);
+         ecc = 0;  // Starting
+         std::vector <bool> check (g.size (), 0); // Check list to check the visited nodes
 
          check [i] = 1;
          for (int j = 0; j < g [i].size (); j++) {
-             q.push (g [i][j]);
+             q.push (g [i][j]); // Start BFS for this vertex
              check [g [i][j]] = 1;
          }
 
+         /*
+          * Perform BFS and increment the eccentricity value as and when you
+          * traverse to a new level
+          */
          while (!q.empty ()) {
 
              temp = q;
@@ -81,17 +88,20 @@
 
                      q.pop ();
                  }
-             ecc++;
+             ecc++; // New level
          }
 
-         E [i] = ecc;
+         E [i] = ecc; // store the vlaue in the vector
      }
 
      return E;
  }
 
  void rnd (std::vector <int> &e) {
-
+      /*
+       *  Radius is the minimum eccentricity
+       *  Diameter is the maxium eccentricity in the graph
+       */
      std::cout << "\n\n\t Radius : " << *std::min_element (e.begin (), e.end ()) << '\n';
      std::cout << "\t Diameter : " << *std::max_element (e.begin (), e.end ()) << '\n';
  }

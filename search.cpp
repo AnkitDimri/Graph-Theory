c44328a468710ef3d26f5ce10aefb80560ec8e16
Ankit <ankitdimri1999@gmail.com>
2019-10-22 18:53:12 +0530

    comments updated

--- search.cpp
+++ search.cpp
@@ -66,13 +66,18 @@
      while (v.size () != 1) {
 
          n = v.size ();
+
+         /* Compare using the terinary and push back into the level vector */
          for (int i = 0; i < v.size ()-1; i += 2)
              temp.push_back ( (v [i] > v [i+1])?v [i]: v [i+1]);
+
+         /* Check in the case with odd sized level so as to not miss the last element of the level */
          if (n%2 != 0)
              temp [temp.size ()-1] = temp [temp.size ()-1] > v [n-1]? temp [temp.size ()-1]:v [n-1];
 
          t.push_back (temp);
 
+         /* prepare the data structures for next iteration */
          v.clear ();
          v = temp;
          temp.clear ();
@@ -81,27 +86,27 @@
 
      v.clear ();
 
-     // First largest element computed in n-1 comparisions
+     /* First largest element computed in n-1 comparisions */
      max1 = t [t.size () - 1][0];
 
-     // Making unordered set of log(n) elements to compare to find second largest element
-     max2 = INT_MIN;
+     max2 = INT_MIN; // Initilize with the smallest number (integer)
+
      for (int i = 0; i < t.size ()-1; i++) {
+
          n = t [i].size ();
-         for (int j = 0; j < t [i].size ()-1; j += 2) {
-             if (t [i][j] == max1) {
-                 //v.push_back (t [i][j+1]);
+         for (int j = 0; j < n; j += 2) {
+
+             if (t [i][j] == max1)
                  if (t [i][j+1] > max2)
                      max2 = t [i][j+1];
-             }
-             else if (t [i][j+1] == max1) {
-                     //v.push_back (t [i][j]);
+
+             else if (t [i][j+1] == max1)
                      if (t [i][j] > max2)
                          max2 = t [i][j];
-             }
+
          }
 
-         /* case to handle when the lenght of vector is odd */
+         /* case to handle when the lenght of level vector is odd */
          if (n%2 != 0) {
              if (t [i][n-2] == max1 || t [0][n-3] == max1) {
                  //v.push_back (t [i][n-1]);
